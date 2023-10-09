dbea6155c788d3fe102977145ca880495ecbd692
Nada <nada.ajinas@deriv.com>
2023-10-09 10:06:16 +0400

    fix: fix merge conflicts

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-export const getCookiesObject = cookies => {};
-=======
 export const getCookiesObject = () => {};
->>>>>>> main
 
 export const getDataObjFromCookies = () => {};
 
-<<<<<<< HEAD
-export const getDataLink = data => {};
-=======
 export const getDataLink = () => {};
->>>>>>> main
 
 export const getCookiesFields = () => [];
