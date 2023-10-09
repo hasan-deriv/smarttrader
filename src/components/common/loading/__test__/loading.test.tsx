67d1a5fdcfffa833c4e8399b415875a4ecf46fae
Nada <nada.ajinas@deriv.com>
2023-10-09 09:34:03 +0400

    fix: run prettier

--- src/components/common/loading/__test__/loading.test.tsx
+++ src/components/common/loading/__test__/loading.test.tsx
@@ -1,11 +1,11 @@
 // Test for passing the github actions
 // TODO should update this test later
-import { describe, expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
-import Loading from "..";
+import { describe, expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import Loading from '..';
 
-describe("Loading component", () =>
-  test("should show loading text", () => {
-    render(<Loading />);
-    expect(screen.getByText("loading")).toBeInTheDocument();
-  }));
+describe('Loading component', () =>
+    test('should show loading text', () => {
+        render(<Loading />);
+        expect(screen.getByText('loading')).toBeInTheDocument();
+    }));
