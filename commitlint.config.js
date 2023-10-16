ebaefcd04a1e0856495b56fbf5f5effcc91a1e6d
Niloofar <niloofar.sadeghi@regentmarkets.com>
2023-10-16 15:03:41 +0800

    docs: added commit formats document

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,10 +4,6 @@ export default {
         'body-max-line-length': [0, 'never', 'Infinity'],
         'subject-case': [0, 'never', 'sentence-case'],
         'footer-max-line-length': [0, 'never', 'Infinity'],
-        'type-enum': [
-            2,
-            'always',
-            ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert'],
-        ],
+        'type-enum': [2, 'always', ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'test']],
     },
 };
