08f62e3104972d59d8f3d9bfd74e038358c3df49
Niloofar <niloofar.sadeghi@regentmarkets.com>
2023-10-16 14:17:41 +0800

    fix: review comments

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
     root: true,
     env: { browser: true, es2020: true },
-    extends: ['@deriv/eslint-config-deriv', 'plugin:@typescript-eslint/recommended'],
+    extends: ['@deriv/eslint-config-deriv', 'plugin:@typescript-eslint/recommended', 'plugin:storybook/recommended'],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
     plugins: ['react-refresh'],
