8a987894b83031d267d8e9b8033b87baa7398a09
hasan-deriv <abuhasan@deriv.com>
2023-10-16 13:10:13 +0800

    Revert "feat: button component"
    This reverts commit 1b34bc8761e119b4d011f1726e9b781e0ed3cca8.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import ErrorBoundary from 'Components/common/error-boundary';
 import Layout from 'Components/layout';
-import { Button } from 'Components/ui/button';
 import AuthProvider from 'Contexts/authProvider';
 
 const App = () => (
     <AuthProvider>
         <ErrorBoundary>
-            <Layout>
-                <Button disabled>add</Button>
-            </Layout>
+            <Layout>App</Layout>
         </ErrorBoundary>
     </AuthProvider>
 );
