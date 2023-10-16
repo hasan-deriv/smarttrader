470644a45b48ef12dd3e62e36c01b3e0ab08c8e8
hasan-deriv <abuhasan@deriv.com>
2023-10-16 14:26:40 +0800

    fix: remove app from commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import ErrorBoundary from 'Components/common/error-boundary';
 import Layout from 'Components/layout';
-import { Button } from 'Components/ui/button';
 import AuthProvider from 'Contexts/authProvider';
 
 const App = () => (
     <AuthProvider>
         <ErrorBoundary>
-            <Layout><Button asChild><a>Button</a></Button></Layout>
+            <Layout>App</Layout>
         </ErrorBoundary>
     </AuthProvider>
 );
