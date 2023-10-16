f723b651de7ae18395695d2adc6cd3e68dd37324
Nada <nada.ajinas@deriv.com>
2023-10-16 15:27:27 +0400

    feat: add modal resusable component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import ErrorBoundary from 'Components/common/error-boundary';
 import Layout from 'Components/layout';
 import AuthProvider from 'Contexts/authProvider';
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from 'Components/ui/modal';
 
 const App = () => (
     <AuthProvider>
         <ErrorBoundary>
-            <Layout>App</Layout>
+            <Layout>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <button>App</button>
+                    </DialogTrigger>
+                    <DialogContent>
+                        <DialogHeader>
+                            <DialogTitle>Title</DialogTitle>
+                            <DialogDescription>description</DialogDescription>
+                        </DialogHeader>
+                        <DialogFooter>
+                            <button>Okay</button>
+                        </DialogFooter>
+                    </DialogContent>
+                </Dialog>
+            </Layout>
         </ErrorBoundary>
     </AuthProvider>
 );
