db2232516305ab5249c14272d2fb51d88124dd6d
Niloofar <niloofar.sadeghi@regentmarkets.com>
2023-10-16 13:55:16 +0800

    chore: precommit configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,12 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: ['@deriv/eslint-config-deriv', 'plugin:@typescript-eslint/recommended', 'plugin:storybook/recommended'],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: ['@deriv/eslint-config-deriv', 'plugin:@typescript-eslint/recommended'],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh'],
+    rules: {
+        'simple-import-sort/imports': 'off',
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+};
---
+++ lint-staged.config.js
@@ -0,0 +1,4 @@
+export default {
+    '*.{js,jsx,ts,tsx,md,html,css,scss}': 'npm run prettier --write',
+    '*.{js,jsx,ts,tsx}': 'npm run lint',
+};
