a153cf05980fecac4fc11033552f5ee37ac4ff73
shayan khaleghparast <iman@regentmarkets.com>
2023-10-18 17:58:38 +0800

    feat: added story for button component

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,9 +1,17 @@
-export const parameters = {
-    actions: { argTypesRegex: '^on[A-Z].*' },
-    controls: {
-        matchers: {
-            color: /(background|color)$/i,
-            date: /Date$/,
+import type { Preview } from '@storybook/react';
+
+import 'Styles/main.css';
+
+const preview: Preview = {
+    parameters: {
+        actions: { argTypesRegex: '^on[A-Z].*' },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
         },
     },
 };
+
+export default preview;
---
+++ stories/Button.stories.ts
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from '../src/components/ui/button';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+const meta = {
+    title: 'Button',
+    component: Button,
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+    tags: ['autodocs'],
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        fullwidth: {
+            options: [true, false],
+            control: { type: 'radio' },
+        },
+        variant: {
+            options: ['contained', 'outlined'],
+            control: { type: 'select' },
+        },
+    },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+    args: {
+        color: 'primary',
+        children: 'test',
+        variant: 'contained',
+        fullwidth: true,
+        className: 'p-2 text-center',
+    },
+};
