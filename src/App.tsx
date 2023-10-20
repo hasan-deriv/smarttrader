fa6622bb0579d5ea3cd80800284c844952774453
“Matin-deriv” <matin@deriv.com>
2023-10-20 11:54:41 +0800

    feat: Clear extra files

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
 import ErrorBoundary from 'Components/common/error-boundary';
 import Layout from 'Components/layout';
 import AuthProvider from 'Contexts/authProvider';
-import { changeLanguage, getInitialLanguage, initializeTranslation } from './translations/i18n';
-import { useTranslation } from 'react-i18next';
-import { SUPPORTED_LANGUAGES } from 'Constants/translations';
 
-initializeTranslation();
-
-const App = () => {
-    const { t } = useTranslation();
-    return (
-        <AuthProvider>
-            <ErrorBoundary>
-                <Layout>{t('Welcome to React')}</Layout>
-                <select className='select' onChange={e => changeLanguage(e.target.value)} value={getInitialLanguage()}>
-                    {Object.entries(SUPPORTED_LANGUAGES).map(([key, value]) => (
-                        <option key={key} value={key}>
-                            {value}
-                        </option>
-                    ))}
-                </select>
-            </ErrorBoundary>
-        </AuthProvider>
-    );
-};
+const App = () => (
+    <AuthProvider>
+        <ErrorBoundary>
+            <Layout>App</Layout>
+        </ErrorBoundary>
+    </AuthProvider>
+);
 
 export default App;
