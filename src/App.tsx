adcf523d4b4f143e336112d668c2241cc0ce11b3
Henry Hein <henry@regentmarkets.com>
2023-10-23 11:18:56 +0800

    fix: remove necessary return

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import ErrorBoundary from 'Components/common/error-boundary';
 import Layout from 'Components/layout';
 
-const App = () => {
-    return (
-        <ErrorBoundary>
-            <Layout>App</Layout>
-        </ErrorBoundary>
-    );
-};
+const App = () => (
+    <ErrorBoundary>
+        <Layout>App</Layout>
+    </ErrorBoundary>
+);
 
 export default App;
