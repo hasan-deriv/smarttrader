3b0022e1ed1196e15c27b532fa93a08c16a2fb2b
Niloofar <niloofar.sadeghi@regentmarkets.com>
2023-10-30 14:36:06 +0800

    refactor: removed unnecessary styles

--- src/components/common/tooltip/index.tsx
+++ src/components/common/tooltip/index.tsx
@@ -11,10 +11,7 @@ const Tooltip = ({ content, children, asChild }: PropsWithChildren<TTooltip>) =>
         <Root>
             <Trigger asChild={asChild}>{children}</Trigger>
             <Portal>
-                <Content
-                    className='data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade rounded-[4px] bg-active p-1 text-xs text-prominent'
-                    sideOffset={5}
-                >
+                <Content className='rounded-[4px] bg-active p-1 text-xs text-prominent' sideOffset={5}>
                     {content}
                     <Arrow className='fill-active' />
                 </Content>
