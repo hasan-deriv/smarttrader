0ea68e327b54f7b06ecbbb36464ae49c8f071e41
hasan-deriv <abuhasan@deriv.com>
2023-10-31 18:09:47 +0800

    fix: app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import ErrorBoundary from 'Components/common/error-boundary';
 import Layout from 'Components/layout';
-import { Button } from 'Components/ui/button';
 
 const App = () => (
     <ErrorBoundary>
-        <Layout>
-            <Button className='min-h-[180px] bg-slate-100 text-5xl'>dfasdfasdf</Button>
-        </Layout>
+        <Layout>App</Layout>
     </ErrorBoundary>
 );
 
