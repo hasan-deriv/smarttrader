a089cdec4dac067611c4c61b7fdade2992420017
hasan-deriv <abuhasan@deriv.com>
2023-10-31 18:07:23 +0800

    feat: added success bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import ErrorBoundary from 'Components/common/error-boundary';
 import Layout from 'Components/layout';
+import { Button } from 'Components/ui/button';
 
 const App = () => (
     <ErrorBoundary>
-        <Layout>App</Layout>
+        <Layout>
+            <Button className='min-h-[180px] bg-slate-100 text-5xl'>dfasdfasdf</Button>
+        </Layout>
     </ErrorBoundary>
 );
 
